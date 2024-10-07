650bff635258366f86f989c70e390037ee92732f
RFCJulioCesar <juliosishui@gmail.com>
2024-10-06 22:47:46 -0600

    tabla genero y migraciones

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Entidades;
+
+namespace WebApplication1
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Genero> Generos => Set<Genero>();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+
+using Microsoft.EntityFrameworkCore;
+using WebApplication1;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +12,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ApplicationDbContext>(opciones => 
+opciones.UseSqlServer("name=DefaultConnection"));        
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
