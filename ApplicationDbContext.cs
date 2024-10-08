457c7c4191b1ed6cc008e35d619432af7bb74d5d
Alain99-Hub <theforestdeath99@gmail.com>
2024-10-07 20:35:38 -0600

    actor created

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -7,6 +7,18 @@ namespace WebApplication1
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Genero>().HasKey(g => g.Id);
+            modelBuilder.Entity<Genero>().Property(g => g.Nombre).HasMaxLength(150);
+
+            modelBuilder.Entity<Actor>().Property(a => a.Nombre).HasMaxLength(150);
+            modelBuilder.Entity<Actor>().Property(a => a.Fortuna).HasMaxLength(150);
+            modelBuilder.Entity<Actor>().Property(a => a.FechaNacimiento).HasColumnType("Date");
+
         }
         public DbSet<Genero> Generos => Set<Genero>();
     }
--- Entidades/Actor.cs
+++ Entidades/Actor.cs
@@ -2,5 +2,11 @@
 {
     public class Actor
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public decimal Fortuna { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+
     }
 }
